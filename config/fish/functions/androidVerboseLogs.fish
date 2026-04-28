da074c061246661e4b23a9ff3cefe1bcd68425c2
Antonio Calabrese <antoniobust@gmail.com>
2026-04-28 19:26:04 +0100

    updates to config

--- config/fish/functions/androidVerboseLogs.fish
+++
@@ -1,4 +0,0 @@
-function androidVerboseLogs --wraps='adb shell setprop log.tag.all VERBOSE' --description 'alias androidVerboseLogs adb shell setprop log.tag.all VERBOSE'
-  adb shell setprop log.tag.all VERBOSE $argv
-        
-end
--- config/fish/functions/vimconfig.fish
+++
@@ -1,4 +0,0 @@
-function vimconfig --wraps='nvim ~/.config/nvim/init.nvim' --wraps='nvim ~/.config/nvim/init.vim' --wraps='nvim ~/.config/nvim' --description 'alias vimconfig=nvim ~/.config/nvim'
-  nvim ~/.config/nvim $argv
-        
-end
